41096169edaff4dc79d0e00f5c7cda2a3dcbff6d
Tanuja Bisht <tannubisht2021@gmail.com>
2026-05-13 14:43:03 +0530

    UI fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -123,7 +123,7 @@ function App() {
                           // fontSize: 20,
                           color: 'green',
                           position: 'absolute',
-                          top: '155px',
+                          top: '160px',
                           left: '403px',
                         }}
                       />
